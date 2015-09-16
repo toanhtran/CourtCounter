900a26ec6224264d1a83fd2aca980385a0a69837
toanhtran <toanh_tran@yahoo.com>
2015-09-16 22:25:32 +0900

    add score methods

---
+++ app/src/main/java/com/example/toanhtran/courtcounter/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.toanhtran.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+/**
+ * Increase the score for Team A BY 3pts.
+ * */
+    public void addThreeForTeamA(View v){
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A BY 2pts.
+     * */
+    public void addTwoForTeamA(View v){
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Increase the score for Team A BY 1pts.
+     * */
+    public void addOneForTeamA(View v){
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+
+    }
+        /**
+         * Displays the given score for Team A.
+         */
+        public void displayForTeamA(int score) {
+            TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+            scoreView.setText(String.valueOf(score));
+        }
+    }
+
