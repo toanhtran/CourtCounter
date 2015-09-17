bfb8e819faa18f66254ebdb9456be33fc37cf9d0
toanhtran <toanh_tran@yahoo.com>
2015-09-17 19:21:45 +0900

    Add blue color styling

--- app/src/main/java/com/example/toanhtran/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/toanhtran/courtcounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,5 +69,46 @@ public class MainActivity extends AppCompatActivity {
             TextView scoreView = (TextView) findViewById(R.id.team_a_score);
             scoreView.setText(String.valueOf(score));
         }
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+     * Increase the score for Team B BY 3pts.
+     * */
+    public void addThreeForTeamB(View v){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B BY 2pts.
+     * */
+    public void addTwoForTeamB(View v){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Increase the score for Team B BY 1pts.
+     * */
+    public void addOneForTeamB(View v){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Reset all scores to 0
+     * */
+    public void resetScore(View v){
+        displayForTeamA(0);
+        displayForTeamB(0);
+
+        scoreForTeamA = 0;
+        scoreForTeamB = 0;
+
+
+    }
     }
 
